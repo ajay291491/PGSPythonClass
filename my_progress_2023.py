5c4490accf665b7d009691eafeb48f92b0ff1046
ajay291491 <ajay291491@gmail.com>
2023-07-21 15:46:52 +0100

    update for class on 21-july-2023

---
+++ my_progress_2023.py
@@ -0,0 +1,31 @@
+# Date : 21-07-2023
+# 
+# > Baic Input and Output
+# my_name = input("Enter Your name : ")
+# my_grade = int(input("Enter Your Grade : ")) 
+# print("Hello", my_name, "Your are Studying at Grade : ", my_grade)
+# print("Hello {}, Your are Studying at Grade : {}".format(my_name, my_grade))
+#
+#
+#
+# Date : 28-07-2023
+> Students should complete this program for first 20 mins
+
+* Write a program to collect an order for your online fruit Shop *
+- You should give option for the list of fruits available
+- Quantity of the Fruits available
+- print a message by what time your give the Fruit with its name, time, 
+quantity, location
+
+Example 
+Fruit Options Available : 
+Apple  - 10 
+Orange - 15
+Grapes - 100
+Kiwi   - 25 
+
+Choose Your Fruit : 
+Choose Quantity : 
+Place to Deliver : 
+
+print("We will Deliver 5 Apple by 20-07-2023 05:00 PM at Madanapalli")
