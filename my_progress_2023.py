7958b78cd1b374fa0a273ce457c4daf171c40a2b
ajay291491 <ajay291491@gmail.com>
2023-08-18 13:26:36 +0100

    Class program for 18-Aug-2023

--- my_progress_2023.py
+++ my_progress_2023.py
@@ -91,102 +91,126 @@
 # else:
 #     print("{} is not available in stock".format(fruit_name))
 
+# Date : 04-Oct-2023
+# # * Write a program to collect an order for your online fruit Shop *
+# #
+#
+# # - You should give option for the list of fruits available
+# # - Quantity of the Fruits available
+#
+# print("Apple = 80")
+# print("Grapes = 50")
+# print("Orange = 50")
+# fruit_name     = input("Enter Your Fruit name : ")
+# fruit_quantity = int(input("Enter Your Quantity : "))
+# if fruit_name == "Apple":
+#     if fruit_quantity <= 80:
+#         print("Fruit - {} is in stock".format(fruit_name))
+#     else:
+#         print("Fruit - {} is not in stock".format(fruit_name))
+# elif fruit_name == "Orange" and fruit_quantity <= 50:
+#     print("Fruit - {} is in stock".format(fruit_name))
+# elif fruit_name == "Grapes" and fruit_quantity <= 50:
+#     print("Fruit - {} is in stock".format(fruit_name))
+# else:
+#     print("{} is not available".format(fruit_name))
+#
+#
+# # - if not available - you should print a message to them it's not available
+# #
 
 
 # 18-Aug-2023 : Built-in Functions
 # These are functions that are provided by Python itself, as mentioned in the previous response. They are available for direct use without requiring any import.
 
-type_of_input = type(123)
-print(type_of_input)
+# # * Addition
+# a = 7
+# b = 2
+# sum = (a + b)
+# print("a + b =  {}".format(sum))
+#
+# # * Substraction
+#
+# a = 7
+# b = 8
+# sum = (a - b)
+# print("a - b =  {}".format(sum))
+#
+# # * Multiplication
+#
+# a = 7
+# b = 8
+# sum = (a * b)
+# print("a * b =  {}".format(sum))
+#
+# # *  Division
+#
+# a = 4
+# b = 2
+# sum = (a / b)
+# print("a / b =  {}".format(sum))
+#
+# # type_of_input = type("cat")
+# # print(type_of_input)
+#
+# # *  Reminder
+# a = 21
+# b = 8
+# sum = (a % b)
+# print("a % b =  {}".format(sum))
+#
+# # Create a calculator program which will ask two numbers as inputs
+# # Also ask for what maths function to be done
+# # Perform the function according to the input and print the results
 
-min_value = min(-10, 10, 20, -100)
-print(min_value)
+# a = 10
+# b = 2
+# c = 5
+# d = 3
+# print("Total : {}".format((a + b) * (c * d)))
 
-max_value = max(-10, 10, 20, -100)
-print(max_value)
+# #* min()
+# min_value = min(-10, 10, 20, -100)
+# print(min_value)
+#
+# #* max()
+# max_value = max(-10, 10, 20, -100)
+# print(max_value)
 
-my_list = range(10)
-for number in my_list:
-    print(number)
-
+# 25-Aug-2023 : Built-in Functions (continue)
 sample_small_name = "peepal grove school "
 print(sample_small_name.upper())
 
-sample_cap_name = "PEEPAL GROVE SCHOOL"
-print(sample_cap_name.lower())
+# * upper
+# sample_cap_name = "PEEPAL GROVE SCHOOL"
+# print(sample_cap_name.lower())
 
-sample_cap_name = "PEEPAL GROVE SCHOOL"
-print(len(sample_cap_name))
+# * lower
+# sample_cap_name = "PEEPAL GROVE SCHOOL"
+# print(len(sample_cap_name))
 
-from datetime import datetime
-current_time= datetime.now()
-print(current_time)
-print(current_time.month)
+# * datetime
+# from datetime import datetime
+# current_time= datetime.now()
+# print(current_time)
+# print(current_time.month)
 
-# Work : Write a Program as below
-# - Check given input is a string
-#  -  if it is a string convert them into capital letter
+# * range
+# my_list = range(10)
+# for number in my_list:
+#     print(number)
 
-
-
-# my_input = input("Enter Your Input : ")
-# if type(my_input) is str:
-#     print(my_input.upper())
-
-# Work : Write a Program as below
-# - Write a program to enter an integer as an input
-# check the number is an even or odd number
-# if even number multiply it by 10 and provide the result
-# if odd number divide by 3
+# List
 
 
-# my_number = int(input("Enter Your number : "))
-# if my_number % 2 == 0:
-#     print(my_number * 10)
-# else:
-#     print(my_number / 3)
+# Loops
 
 
-# Work : Write a Program as below
-# - Write a program to enter an integer as an input
-# check the number is an even or odd number
-# if even number multiply it by 10 and provide the result
-# if odd number divide by 3
+# Dictionary
 
 
-# 18-Aug-2023 : List
+# Loops with dict
 
 
 
 
-
-
-
-
-
-# * Write a program to collect an order for your online fruit Shop *
-#
-
-# - You should give option for the list of fruits available
-# - Quantity of the Fruits available
-
-print("Apple = 80")
-print("Grapes = 50")
-print("Orange = 50")
-fruit_name     = input("Enter Your Fruit name : ")
-fruit_quantity = int(input("Enter Your Quantity : "))
-if fruit_name == "Apple":
-    if fruit_quantity <= 80:
-        print("Fruit - {} is in stock".format(fruit_name))
-    else:
-        print("Fruit - {} is not in stock".format(fruit_name))
-elif fruit_name == "Orange" and fruit_quantity <= 50:
-    print("Fruit - {} is in stock".format(fruit_name))
-elif fruit_name == "Grapes" and fruit_quantity <= 50:
-    print("Fruit - {} is in stock".format(fruit_name))
-else:
-    print("{} is not available".format(fruit_name))
-
-
-# - if not available - you should print a message to them it's not available
-# - You must have at least 3 types of fruits available in your stock
